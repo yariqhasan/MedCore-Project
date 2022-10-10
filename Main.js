ac9f8f5cdd7edb6fd815982616b5b853676e454a
Laglicious <60292339+Laglicious@users.noreply.github.com>
2022-10-10 17:31:07 -0400

    main.js more dynamic for team use
    Change the variables to what you need them to be.

--- Main.js
+++ Main.js
@@ -2,49 +2,51 @@ const mysql = require("mysql");
 const express = require("express");
 const bodyParser = require("body-parser");
 const encoder = bodyParser.urlencoded();
+// change to an available localhost port
+const portnum = 1120;
+const schemaName = "hospitaldb";
+const dbPassword = "example";
 
 const app = express();
 app.use("/assets",express.static("assets"));
 
-
+//establish connection to your local database
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Fcbayern1",
-    database: "clinic_data"
+    password: `${dbPassword}`,
+    database: `${schemaName}`,
 });
 
 // connect to the database
 connection.connect(function(error){
     if (error) throw error
-    else console.log("connected to the database successfully!")
+    else console.log("connected to the database successfully!");
 });
 
-
-app.get("/Patientlogin",function(req,res){
+//index loads up to localhost:#port
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/index.html");
+})
+app.get("/PatientLogin.html",function(req,res){
     res.sendFile(__dirname + "/PatientLogin.html");
 })
-
-
-app.post("/log",encoder, function(req,res){
+app.post("/",encoder, function(req,res){
     var username = req.body.username;
     var password = req.body.password;
 
-    connection.query("select * from clinic_data.patient_login where username = ? and password = ?",[username,password],function(error,results,fields){
+    connection.query(`select * from ${schemaName}.patient_login where username = ${username} and password = ${password}`,[username,password],function(error,results,fields){
         if (results.length > 0) {
-            res.redirect("/Profile");
+            res.redirect("/Profile.html");
         } else {
-            res.redirect("/PatientLogin");
+            res.redirect("/PatientLogin.html");
         }
         res.end();
     })
 })
-
 // when login is success
-app.get("/Profile",function(req,res){
+app.get("/Profile.html",function(req,res){
     res.sendFile(__dirname + "/Profile.html")
 })
-
-
 // set app port 
-app.listen(4000);
+app.listen(portnum);
