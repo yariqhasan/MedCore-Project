60b469d01e25e23a95e689bc952c3a67172d8fd4
Schmaltz <SiriusVisualsBusiness@gmail.com>
2022-11-02 20:56:44 -0400

    sus github

--- assets/js/appointment.js
+++ assets/js/appointment.js
@@ -27,7 +27,7 @@ $(document).ready(function () {
   })
 })
 
-function myFunction() {
+function createRow() {
   var table = document.getElementById("appointment-table")
   var row = table.insertRow(0)
   var cell1 = row.insertCell(0)
@@ -35,11 +35,44 @@ function myFunction() {
   var cell3 = row.insertCell(2)
   var cell4 = row.insertCell(3)
   var cell5 = row.insertCell(4)
-  cell1.innerHTML = ""
+  var cell6 = row.insertCell(5)
+
+  var checkbox = document.createElement("input")
+  checkbox.type = "checkbox"
+  checkbox.name = "name"
+  checkbox.value = "value"
+  checkbox.id = "id"
+
+  cell1.appendChild(checkbox)
   cell2.innerHTML = document.getElementById("name").value
   cell3.innerHTML = document.getElementById("email").value
   cell4.innerHTML = document.getElementById("address").value
   cell5.innerHTML = document.getElementById("phone").value
+  console.log(table)
+
+  var rows = table.getElementsByTagName("tr")
+  for (var i = 0; i <= rows.length; i++) {
+    if (i % 2 == 0) {
+      rows[i].style.backgroundColor = "#F5F5F5"
+    } else {
+      rows[i].style.backgroundColor = "#FFFFFF"
+    }
+  }
+}
+
+function removeSelected(table) {
+  for (var rowi = table.rows.length; rowi-- > 0; ) {
+    var row = table.rows[rowi]
+    var inputs = row.getElementsByTagName("input")
+    for (var inputi = inputs.length; inputi-- > 0; ) {
+      var input = inputs[inputi]
+
+      if (input.type === "checkbox" && input.checked) {
+        row.parentNode.removeChild(row)
+        break
+      }
+    }
+  }
 }
 
 function openForm() {
