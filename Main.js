5d55062e464484c20932315db528d496d3885625
Alex M <alexander.muszynski@gmail.com>
2022-11-18 19:24:57 -0500

    ownid

--- Main.js
+++ Main.js
@@ -1,7 +1,6 @@
-const mysql = require("mysql");
 const express = require("express");
 const bodyParser = require("body-parser");
-const encoder = bodyParser.urlencoded();
+const encoder = bodyParser.urlencoded({extended: true});
 
 const app = express();
 app.use(express.static('assets'))
@@ -9,88 +8,61 @@ app.use('/css',express.static(__dirname+ 'assets/css'));
 app.use('/js',express.static(__dirname+ 'assets/js'));
 app.use('/img',express.static(__dirname+ 'assets/img'));
 app.use('/vendor',express.static(__dirname+ 'assets/vender'));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
 
-
+//include folder
+const myFilePath = `/home/muszyn/Documents`
+const assets = require(`${myFilePath}/MedCore-Project/assets/js/queryFunctions`);
 //set views
 app.set('views', './views');
 app.set('view engine', 'ejs');
-
-
 // change to an available localhost port
 // Can use Resource Monitor (Windows) to find available localports
 const localHostPortNum = 1120;
-const schemaName = "clinic_data";
-const dbPassword = "Dy3rmak3r";
-
-// set app port 
 app.listen(localHostPortNum);
-//establish connection to your local database
-const connection = mysql.createConnection({
-    host: "127.0.0.1",
-    user: "admin",
-    password: `${dbPassword}`,
-    database: `${schemaName}`,
-    port: 3306,
-});
 
-// connect to the database
-connection.connect(function(error){
-    if (error) throw error
-    else console.log("connected to the database successfully!")
-});
-
-
-app.get("",(req,res) => {
-    res.render('index');
-});
-
-app.post('/getMedicineInfo', async (req,res)=>{
-    const query = `select * from ${schemaName}.medicine`;
-    connection.query(query, function(error,results,fields){
-        if(error){
-            console.log(error);
-            throw error;
-        }
-        console.log(results[0].objid);
-    });
-    res.json({results});
-});
-
-app.post('/ownId/setOwnIDDataByLoginId', async (req, res) => {
-    const email = req.body.loginId; //The unique id of a user in your database, usually email or phone
-    const ownIdData = req.body.ownIdData; //OwnID authentication information as string
-    const user = await user.findOne({ email: email }).exec();
-    user.ownIdData = ownIdData;
-    await user.save();
+//#region OwnID
+app.post('/setOwnIDDataByLoginId', async (req, res) => {
+    const user = assets.getUserByEmail(req.body.loginId);
+    if(!user){
+        console.log(req.body.loginId);
+        return res.json({errorCode: 404})
+    }
+    assets.saveUserByEmail(req.body.loginId,req.ownIdData);
     return res.sendStatus(204);
 });
 
-app.post('/ownId/getOwnIDDataByLoginId', async (req, res) => {
-    const email = req.body.loginId; //The unique id of a user in your database, usually email or phone
-    const user = await user.findOne({ email: email }).exec();
-    if (!user) { return res.json({ errorCode: 404 }) } //Error code when user doesn't exist
-    res.json({ ownIdData: user.ownIdData }) //OwnID authentication information as string
+app.post('/getOwnIDDataByLoginId', async (req, res) => {
+    const user = assets.getUserByEmail(req.body.loginId);
+    if(!user){
+        return res.json({errorCode: 404})
+    }
+    res.json({ownIdData: user.ownIdData})
 });
 
-app.post('/ownId/getSessionByLoginId', async (req, res) => {
+app.post('/getSessionByLoginId', async (req, res) => {
     const sign = require('jwt-encode');
-
-    const email = req.body.loginId; //The unique id of a user in your database, usually email or phone
-    const user = await user.findOne({ email: email }).exec();
-    const jwt = sign({ email: user.email }, 'secret');
-    return res.json({ token: jwt });
+    const user = assets.getUserByEmail(req.body.loginId);
+    if (!user) {
+        return res.json({errorCode: 404})
+    }
+    const jwt = sign({email: user.email}, 'secret');
+    res.json({token: jwt});
 });
+//#endregion
 
-
-
-
+//#region Login
 app.post("/login",encoder, function(req,res){
     var username = req.body.username;
     var password = req.body.password;
     var email = req.body.email;
-    const loginQuery = `select * from ${schemaName}.login where username = "${username}" and password = "${password}"`
-    const employeeLoginQuery = `select * from ${schemaName}.login where username = "${username}" and password = "${password}"`
-    connection.query(loginQuery, function(error,results,fields){
+    const loginQuery = `select * from ${assets.schemaName}.login where username = "${username}" and password = "${password}"`
+    const employeeLoginQuery = `select * from ${assets.schemaName}.login where username = "${username}" and password = "${password}"`
+    assets.connection.query(loginQuery, function(error,results,fields){
         if(username.length == 0 || password.length == 0){
             res.redirect("PatientLogin");
             return;
@@ -107,8 +79,9 @@ app.post("/login",encoder, function(req,res){
         res.end();
     })
 })
+//#endregion
 
-//New Patient Entry Form
+//#region New Entry
 
 app.post("/new_entry",encoder, function(req,res){
     var PID = req.body.PID;
@@ -119,21 +92,24 @@ app.post("/new_entry",encoder, function(req,res){
     var email_addr = req.body.email_addr;
     var sex = req.body.sex;
     
-    var sql = `insert into ${schemaName}.patient (PID, first_name, last_name, contact_num, address, email_addr,sex) Values ?`;
+    var sql = `insert into ${assets.schemaName}.patient (PID, first_name, last_name, contact_num, address, email_addr,sex) Values ?`;
     var values = [[PID,first_name,last_name,contact_num,address,email_addr,sex]];
-    connection.query(sql, [values], function (err, result) {
+    assets.connection.query(sql, [values], function (err, result) {
         if (err) throw err;
         console.log("Number of records inserted: " + result.affectedRows);
         res.redirect("/Patient_entry");
    });
 
 });
+//#endregion
+
+//#region CreateUser
 app.post("/", encoder, function(req, res){
     var email = req.body.email;
     var username = req.body.username;
     var password = req.body.password;
     var confirmPassword = req.body.confirmPassword;
-    const createAccountQuery = `INSERT INTO ${schemaName}.login (username, password, email) VALUES ("${username}", "${password}", "${email}")`
+    const createAccountQuery = `INSERT INTO ${assets.schemaName}.login (username, password, email) VALUES ("${username}", "${password}", "${email}")`
     if(!email | !username | !password | !confirmPassword){
         res.redirect(`/RegisterAccount`)
     }
@@ -142,7 +118,7 @@ app.post("/", encoder, function(req, res){
         console.log(error)
         res.redirect(`/RegisterLogin`)
     }
-    connection.query(createAccountQuery, function(error, results, fields){
+    assets.connection.query(createAccountQuery, function(error, results, fields){
         if(!error) {
             console.log(`User ${username} created successfully`)
             res.redirect(`/PatientLogin`)
@@ -155,11 +131,12 @@ app.post("/", encoder, function(req, res){
     })
 
 })
+//#endregion
 
 
-//Fetch data from SQL to Table
+//#region PatientList
 app.get("/PatientList", (req, res) =>{
-    connection.query('SELECT * FROM clinic_data.patient ',(err, rows) => {
+    assets.connection.query(`SELECT * FROM ${assets.schemaName}.patient `,(err, rows) => {
          if (err) throw err;
          console.log(rows);
          res.render("PatientList",{
@@ -167,8 +144,12 @@ app.get("/PatientList", (req, res) =>{
          });
     })
  });
+ //#endregion
 
-// when login is success
+//#region Routes
+app.get("",(req,res) => {
+    res.render('index');
+});
 app.get("/admin_login",(req,res) => {
     res.render('admin_login');
 });
@@ -210,3 +191,4 @@ app.get("/RegisterAccount",(req,res) => {
 app.get("/DoctorBilling", (req,res) => {
     res.render('DoctorBilling');
 });
+//#endregion
---
+++ assets/js/queryFunctions.js
@@ -0,0 +1,35 @@
+const mysql = require("mysql2");
+
+const localHostPortNum = 1120;
+const schemaName = "clinic_data";
+const dbPassword = "Dy3rmak3r";
+
+const connection = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "admin",
+    password: `${dbPassword}`,
+    database: `${schemaName}`,
+    port: 3306,
+});
+// connect to the database
+connection.connect(function(error){
+    if (error) throw error
+    else console.log(`Connected to ${schemaName}!`)
+});
+async function getUserByEmail(loginId){
+    var query = `select login.username from ${schemaName}.login where email = "${loginId}"`
+    const results = await connection.promise().query(query)
+    return results[0]
+};
+async function saveUserByEmail(loginId, ownIdData){
+    var query = `UPDATE ${schemaName}.login SET ownIdData = "${ownIdData}" WHERE email = "${loginId}"`
+    const results = await connection.promise().query(query)
+    return results
+};
+
+module.exports = {
+    getUserByEmail,
+    saveUserByEmail,
+    connection,
+    schemaName,
+}
